10f239386eb2b844f3a466e50a50b17448d1b1b0
dakuto <dakuto.h.1978@gmail.com>
2026-05-06 15:09:53 +0900

    アナリティクスを追加設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,21 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout
-      isTopPage={pageProps.isTopPage}
-      allTags={pageProps.allTags}
-      latestPosts={pageProps.latestPosts}
-      allPosts={pageProps.allPosts}
-      hideLayout={pageProps.hideLayout}
-    >
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout
+        isTopPage={pageProps.isTopPage}
+        allTags={pageProps.allTags}
+        latestPosts={pageProps.latestPosts}
+        allPosts={pageProps.allPosts}
+        hideLayout={pageProps.hideLayout}
+      >
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+    </>
   );
 }
